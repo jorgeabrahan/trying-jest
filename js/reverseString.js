a7c6e79185bd711a3aec7f0f25b46c97c17912aa
Jorge <jorge24abrahan@gmail.com>
2023-01-24 12:12:48 -0600

    Create function to reverse string

--- js/reverseString.js
+++ js/reverseString.js
@@ -1,5 +1,9 @@
-function reverseString() {
-  return '';
+function reverseString(string) {
+  const reversed = [];
+  for (let i = string.length; i >= 0; i-=1) {
+    reversed.push(string[i]);
+  }
+  return reversed.join('');
 }
 
 module.exports = reverseString;
