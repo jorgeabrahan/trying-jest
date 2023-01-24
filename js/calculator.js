5bd0d351f8d6d239f03b8b73da5957483b52bb55
Jorge <jorge24abrahan@gmail.com>
2023-01-24 12:40:59 -0600

    Create calculator tests

--- js/calculator.js
+++ js/calculator.js
@@ -1,28 +1,25 @@
 class Calculator {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  add(x, y) {
+    return x + y;
   }
 
-  add() {
-    return this.x + this.y;
+  subtract(x, y) {
+    return x - y;
   }
 
-  subtract() {
-    return this.x - this.y;
-  }
-
-  divide() {
+  divide(x, y) {
     if(x === 0 && y !== 0)
       throw new Error('Invalid!');
     
     if((x === 0 && y === 0) || (x !== 0 && y === 0))
       throw new Error('Not Allowed!');
 
-    return this.x / this.y;
+    return x / y;
   }
 
-  multiply() {
-    return this.x * this.y;
+  multiply(x, y) {
+    return x * y;
   }
 }
+
+module.exports = Calculator;
