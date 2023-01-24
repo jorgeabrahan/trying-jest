b4dc2952ab1854a7ff501c26bdcb9d3506492bb2
Jorge <jorge24abrahan@gmail.com>
2023-01-24 11:44:47 -0600

    Add new functionality if string is less than 1 or more than 10 characters and create tests

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,8 @@
-const stringLength = (string) => {
-
+function stringLength(string) {
+  if (string.length < 1 || string.length > 10) {
+    throw new Error('String does not meet the conditions (at least 1 character and not more than 10)');
+  }
+  return string.length;
 }
 
-export default stringLength;
+module.exports = stringLength;
--- js/task-1.test.js
+++ js/task-1.test.js
@@ -1,9 +1,17 @@
-import stringLength from "./task-1.js";
+const stringLength = require('./task-1');
 
-it("Should return 6 as the length of the string: border", () => {
-  expect(stringLength("border")).toBe(6);
+it('Should return 6 as the length of the string: border', () => {
+  expect(stringLength('border')).toBe(6);
 });
 
-it("Should return 8 as the length of the string: notebook", () => {
-  expect(stringLength("notebook")).toBe(8);
+it('Should return 8 as the length of the string: notebook', () => {
+  expect(stringLength('notebook')).toBe(8);
+});
+
+it('Should return an error because the string is empty', () => {
+  expect(() => stringLength('')).toThrow(Error);
+});
+
+it('Should return an error because the string has more than 10 characters', () => {
+  expect(() => stringLength('accomplishment')).toThrow(Error);
 });
